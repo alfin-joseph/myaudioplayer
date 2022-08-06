68521c209ce8a15672109dd4a28ecec8e0df2e41
alfin-joseph <91449714+alfin-joseph@users.noreply.github.com>
2022-08-07 02:02:16 +0530

    error correctio

--- myaudio/myaudio/asgi.py
+++ myaudio/myaudio/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myaudio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DEFAULT_SETTINGS_MODULE')
 
 application = get_asgi_application()
--- myaudio/myaudio/wsgi.py
+++ myaudio/myaudio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myaudio.myaudio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DEFAULT_SETTINGS_MODULE')
 
 application = get_wsgi_application()
